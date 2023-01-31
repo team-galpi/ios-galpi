3234af2f8d08c15dc9ab7b9366f6a25b9784148a
Ellen J <jcrescent61@gmail.com>
2023-01-31 20:08:30 +0900

    #2 feat: TextLineView 구현

---
+++ galpi/galpi/GalpiPost/TextLineView.swift
@@ -0,0 +1,36 @@
+//
+//  TextLineView.swift
+//  galpi
+//
+//  Created by Ellen J on 2023/01/31.
+//
+
+import SwiftUI
+
+struct TextLineView: View {
+    var body: some View {
+        VStack(alignment: .center) {
+            Rectangle()
+                .frame(width: AppConstraints.width, height: 1, alignment: .center)
+                .foregroundColor(Color(UIColor(red: 242/255, green: 242/255, blue: 242/255, alpha: 1)))
+            Spacer(minLength: 40)
+            Rectangle()
+                .frame(width: AppConstraints.width, height: 1, alignment: .center)
+                .foregroundColor(Color(UIColor(red: 242/255, green: 242/255, blue: 242/255, alpha: 1)))
+            Spacer(minLength: 40)
+            Rectangle()
+                .frame(width: AppConstraints.width, height: 1, alignment: .center)
+                .foregroundColor(Color(UIColor(red: 242/255, green: 242/255, blue: 242/255, alpha: 1)))
+            Spacer(minLength: 40)
+            Rectangle()
+                .frame(width: AppConstraints.width, height: 1, alignment: .center)
+                .foregroundColor(Color(UIColor(red: 242/255, green: 242/255, blue: 242/255, alpha: 1)))
+        }
+    }
+}
+
+struct TextLineView_Previews: PreviewProvider {
+    static var previews: some View {
+        TextLineView()
+    }
+}
