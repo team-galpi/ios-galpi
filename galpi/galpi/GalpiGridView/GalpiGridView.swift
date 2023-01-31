5888cdd9f4db7b4a8439427bdc59033eada7e1b2
minsson <mskaang@gmail.com>
2023-01-31 17:35:08 +0900

    #1 feat: GalpiGridView.swift 생성

---
+++ galpi/galpi/GalpiGridView/GalpiGridView.swift
@@ -0,0 +1,20 @@
+//
+//  GalpiGridView.swift
+//  galpi
+//
+//  Created by minsson on 2023/01/31.
+//
+
+import SwiftUI
+
+struct GalpiGridView: View {
+    var body: some View {
+        Text("")
+    }
+}
+
+struct GalpiGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        GalpiGridView()
+    }
+}
