ffd87b941563fed6048facde9c0b1b14feef6782
minsson <mskaang@gmail.com>
2023-01-31 17:05:31 +0900

    프로젝트 생성

---
+++ galpi/galpi/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  galpi
+//
+//  Created by minsson on 2023/01/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ galpi/galpi/galpiApp.swift
@@ -0,0 +1,17 @@
+//
+//  galpiApp.swift
+//  galpi
+//
+//  Created by minsson on 2023/01/31.
+//
+
+import SwiftUI
+
+@main
+struct galpiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
