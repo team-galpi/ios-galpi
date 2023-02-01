0347eb67b25344ed6ab92cbbb973c5d68237d03e
minsson <mskaang@gmail.com>
2023-02-01 13:45:01 +0900

    #1 feat: 기존에 작성해놓은 Galpi를 볼 수 있는 GalpiGridView 구현

--- galpi/galpi/GalpiGridView/GalpiGridView.swift
+++ galpi/galpi/GalpiGridView/GalpiGridView.swift
@@ -8,8 +8,34 @@
 import SwiftUI
 
 struct GalpiGridView: View {
+    
+    let gridColumn: [GridItem] = [GridItem(.adaptive(minimum: 140), spacing: 0)]
+    
+    @State private var selectedGalpi = Galpi(title: "", image: "", date: Date.now, author: "", publisher: "", quotes: "", opinion: "")
+    @State private var shouldPresentGalpiReadView = false
+    
     var body: some View {
-        Text("")
+        HStack {
+            ScrollView {
+                LazyVGrid(columns: gridColumn, spacing: 37) {
+                    ForEach(dummyGalpis, id: \.self) { galpi in
+                        GalpiGridCellView(galpi: galpi)
+                            .onTapGesture {
+                                selectedGalpi = galpi
+                                shouldPresentGalpiReadView.toggle()
+                            }
+                            .fullScreenCover(isPresented: $shouldPresentGalpiReadView) {
+                                Button {
+                                    shouldPresentGalpiReadView.toggle()
+                                } label: {
+                                    // TODO: GalpiReadView에 galpi 전달하여 열기
+                                }
+                            }
+                    }
+                }
+            }
+        }
+        .padding(EdgeInsets(top: 78, leading: 20, bottom: 0, trailing: 20))
     }
 }
 
