edcef6a6fce4825224f13d8352002085bac652c3
minsson <mskaang@gmail.com>
2023-02-01 13:28:48 +0900

    #1 feat: Galpi 모델 객체 생성

---
+++ galpi/galpi/Galpi.swift
@@ -0,0 +1,18 @@
+//
+//  Galpi.swift
+//  galpi
+//
+//  Created by minsson on 2023/02/01.
+//
+
+import Foundation
+
+struct Galpi: Hashable {
+    var title: String
+    var image: String
+    var date: Date
+    var author: String
+    var publisher: String
+    var quotes: String
+    var opinion: String
+}
