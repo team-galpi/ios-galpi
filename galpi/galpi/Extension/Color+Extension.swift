5136880ab8bf3261417a3836673dd1b354c0a681
Ellen J <jcrescent61@gmail.com>
2023-02-09 22:39:44 +0900

    #5 feat: RGB값 편리하게 입력할 수 있는 Color 이니셜라이저 구현

---
+++ galpi/galpi/Extension/Color+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  UIColor+Extension.swift
+//  galpi
+//
+//  Created by Ellen J on 2023/02/09.
+//
+
+import SwiftUI
+
+extension Color {
+    init(r: CGFloat, g: CGFloat, b: CGFloat, a: CGFloat = 1.0) {
+        self.init(red: r/255, green: g/255, blue: b/255, opacity: a)
+    }
+}
