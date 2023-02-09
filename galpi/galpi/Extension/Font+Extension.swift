bd502405e850470897eb83a282c3479570fa3f8b
Ellen J <jcrescent61@gmail.com>
2023-02-09 22:46:00 +0900

    #5 feat: GDS폴더 생성 및 JejuMyeongjoOTF 폰트 추가

--- galpi/galpi/Extension/Font+Extension.swift
+++ galpi/galpi/Extension/Font+Extension.swift
@@ -4,3 +4,14 @@
 //
 //  Created by Ellen J on 2023/02/09.
 //
+
+import SwiftUI
+
+extension Font {
+    
+    static func jejuMyeongjo(size: CGFloat) -> Font {
+        return Font.custom("JejuMyeongjoOTF", size: size)
+    }
+    
+    
+}
