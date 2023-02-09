86648765ee5ee1a2493c731f0a5313f395d63ff0
minsson <mskaang@gmail.com>
2023-02-09 19:35:39 +0900

    #6 feat: 탭바에 모든 화면을 포함하는 TabBarContainerView 파일 생성

---
+++ galpi/galpi/Domain/Common/TabBarContainerView.swift
@@ -0,0 +1,24 @@
+//
+//  TabBarContainerView.swift
+//  galpi
+//
+//  Created by minsson on 2023/02/09.
+//
+
+import SwiftUI
+
+struct TabBarContainerView: View {
+    
+    var body: some View {
+        Text("")
+    }
+    
+}
+
+struct TabBarContainerView_Previews: PreviewProvider {
+    
+    static var previews: some View {
+        TabBarContainerView()
+    }
+    
+}
