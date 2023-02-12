d03dad7253827569ef0ddedb7f84e63378def2d8
minsson <mskaang@gmail.com>
2023-02-12 23:59:07 +0900

    #9 feat: AuthenticationViewModel 파일 생성
    - LoginView에서 사용할 뷰모델입니다.

---
+++ galpi/galpi/Domain/Login/AuthenticationViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  AuthenticationViewModel.swift
+//  galpi
+//
+//  Created by minsson on 2023/02/12.
+//
+
+import Foundation
+
+final class AuthenticationViewModel: ObservableObject {
+    
+}
