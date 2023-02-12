d2959e785f5be682466556f4f953592356e7ce48
minsson <mskaang@gmail.com>
2023-02-12 23:37:17 +0900

    #9 feat: LoginView 파일 생성
    - Sign in with Apple 버튼이 위치할 화면입니다.
    - 추후 다른 소셜 로그인 기능이 추가될 수 있습니다.

---
+++ galpi/galpi/Domain/Login/LoginView.swift
@@ -0,0 +1,24 @@
+//
+//  LoginView.swift
+//  galpi
+//
+//  Created by minsson on 2023/02/12.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    
+    var body: some View {
+        Text("")
+    }
+    
+}
+
+struct LoginView_Previews: PreviewProvider {
+    
+    static var previews: some View {
+        LoginView()
+    }
+    
+}
