bce879a83b4cd00282e3538d1756799993b4ae39
minsson <mskaang@gmail.com>
2023-02-18 01:56:02 +0900

    #9 refactor: FirebaseLoginManager의 signIn 메서드 구현
    - AppleLoginManager로부터 FirebaseAuth에 필요한 데이터를 컴플리션 핸들러로 받아와, FirebaseAuth를 진행하는 메서드입니다.
        - 이 메서드는 LoginViewModel에서 로그인 버튼이 탭 되면 호출할 메서드입니다.
    - Firebase 로그인이 완료되면 컴플리션 핸들러로 성공 여부를 반환합니다.
    - 소셜 로그인 관련 확장성을 대비하여 AppleLoginManager는 추후 SocialLoginManager로 추상화하려고 합니다.
        - SocialLoginManager 프로토콜을 채택한 객체라면 어떤 서비스든 FirebaseAuth로 Firebase 로그인을 할 수 있게 됩니다.

--- galpi/galpi/Domain/Common/FirebaseLoginManager.swift
+++ galpi/galpi/Domain/Common/FirebaseLoginManager.swift
@@ -9,4 +9,35 @@ import FirebaseAuth
 
 final class FirebaseLoginManager {
     
+    func signIn(with concreteSocialLoginManager: AppleLoginManager, completion: @escaping (Result<Bool, Error>) -> Void) {
+        concreteSocialLoginManager.signIn { credentialResult in
+            switch credentialResult {
+            case .success(let socialAuthcredential):
+                let firebaseCredential = self.firebaseCredential(socialAuthCredential: socialAuthcredential)
+                
+                Auth.auth().signIn(with: firebaseCredential) { (authResult, error) in
+                  if let error {
+                    print(error)
+                    return
+                  }
+   
+                    completion(.success(true))
+                }
+    
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    private func firebaseCredential(socialAuthCredential: SocialLoginCredential) -> OAuthCredential {
+        let firebaseCredential = OAuthProvider.credential(
+            withProviderID: socialAuthCredential.providerID,
+            idToken: socialAuthCredential.token,
+            rawNonce: socialAuthCredential.nonce
+        )
+        
+        return firebaseCredential
+    }
+    
 }
