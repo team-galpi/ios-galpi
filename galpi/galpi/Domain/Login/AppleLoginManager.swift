d7ec7322f03414eabca0531487d471bad21cd897
minsson <mskaang@gmail.com>
2023-02-18 00:06:26 +0900

    #9 refactor: AppleLoginManager 객체 생성 및 기존 뷰모델의 역할 이양
    - 뷰 모델의 역할이 과중하여 별도의 객체로 분리했습니다.
    - 기존 코드에서는 iOS14부터 SwiftUI에서 이용 가능한 SignInWithAppleButton 객체를 사용했습니다.
    - 처음에는 구현이 매우 간단해 사용했지만, 뷰에서 비즈니스 로직을 실행하고, 뷰가 뷰모델에게 데이터를 전달하게 되는 문제가 있었습니다.
    - 더 좋은 구조 분리를 위해 코드 구현이 번거롭지만 FirebaseAuth에서 안내하는 기존의 방식으로 리팩토링하기 시작했습니다.

---
+++ galpi/galpi/Domain/Login/AppleLoginManager.swift
@@ -0,0 +1,71 @@
+//
+//  AppleLoginManager.swift
+//  galpi
+//
+//  Created by minsson on 2023/02/17.
+//
+
+import Foundation
+import AuthenticationServices
+import FirebaseAuth
+
+final class AppleLoginManager: NSObject {
+    
+    private var currentNonce: String?
+    
+    func startSignInWithAppleFlow() {
+        let authSecurityService = AuthSecurityService()
+        let nonce = authSecurityService.randomNonceString()
+        currentNonce = nonce
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        let request = appleIDProvider.createRequest()
+        request.requestedScopes = [.fullName, .email]
+        request.nonce = authSecurityService.sha256(nonce)
+        
+        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+        authorizationController.delegate = self
+        authorizationController.performRequests()
+    }
+    
+}
+
+extension AppleLoginManager: ASAuthorizationControllerDelegate {
+    
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+      if let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential {
+        guard let nonce = currentNonce else {
+          fatalError("Invalid state: A login callback was received, but no login request was sent.")
+        }
+        guard let appleIDToken = appleIDCredential.identityToken else {
+          print("Unable to fetch identity token")
+          return
+        }
+        guard let idTokenString = String(data: appleIDToken, encoding: .utf8) else {
+          print("Unable to serialize token string from data: \(appleIDToken.debugDescription)")
+          return
+        }
+        // Initialize a Firebase credential.
+        let credential = OAuthProvider.credential(withProviderID: "apple.com",
+                                                  idToken: idTokenString,
+                                                  rawNonce: nonce)
+        // Sign in with Firebase.
+        Auth.auth().signIn(with: credential) { (authResult, error) in
+          if let error {
+            // Error. If error.code == .MissingOrInvalidNonce, make sure
+            // you're sending the SHA256-hashed nonce as a hex string with
+            // your request to Apple.
+            print(error.localizedDescription)
+            return
+          }
+          // User is signed in to Firebase with Apple.
+          // ...
+        }
+      }
+    }
+
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+      // Handle error.
+      print("Sign in with Apple errored: \(error)")
+    }
+    
+}
