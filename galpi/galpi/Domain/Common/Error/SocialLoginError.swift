f2d2d10ef01e99d123b83f30eab3806d948b86db
minsson <mskaang@gmail.com>
2023-03-14 23:41:10 +0900

    #12 feat: SocialLoginError 케이스 추가

--- galpi/galpi/Domain/Common/Error/SocialLoginError.swift
+++ galpi/galpi/Domain/Common/Error/SocialLoginError.swift
@@ -9,12 +9,18 @@ enum SocialLoginError: GalpiErrorProtocol {
     
     case canceled
     case internalError
+    case identityToken
+    case idTokenString
     
     var code: String {
         switch self {
         case .canceled:
             return "A100"
         case .internalError:
+            return "A200"
+        case .identityToken:
+            return "A300"
+        case .idTokenString:
             return "A400"
         }
     }
