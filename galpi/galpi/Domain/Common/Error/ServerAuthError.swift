a807f7efbe777cd27abb56305f9becb1a7fd7fbc
minsson <mskaang@gmail.com>
2023-03-15 00:21:27 +0900

    #12 feat: ServerAuthError 정의
    - case 중 socialLoginError(SocialLoginError)
        - FirebaseAuthService에서 socialLoginManager의 signIn 메서드를 실행할 때 Result 타입으로 SocialLoginError를 전달 받아 LoginViewModel로 넘겨주기 위해 사용합니다.

---
+++ galpi/galpi/Domain/Common/Error/ServerAuthError.swift
@@ -0,0 +1,31 @@
+//
+//  ServerAuthError.swift
+//  galpi
+//
+//  Created by minsson on 2023/03/15.
+//
+
+enum ServerAuthError: GalpiErrorProtocol {
+
+    case internalError
+    case socialLoginError(SocialLoginError)
+    
+    var code: String {
+        switch self {
+        case .internalError:
+            return "F100"
+        case .socialLoginError(let error):
+            return error.code
+        }
+    }
+    
+    var errorReason: String {
+        switch self {
+        case .internalError:
+            return "Server Auth 내부 에러"
+        case .socialLoginError(let error):
+            return error.errorReason
+        }
+    }
+    
+}
